b238e7166cb7e872220d13e27697d007a205820b
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-09 14:44:19 +0000

    feat(dependences installed and models almost complete)

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
         }
+
+class BlacklistToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
